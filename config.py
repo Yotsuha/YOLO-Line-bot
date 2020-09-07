8fbea8b37889de3fbb8bbcd5372707719e573bff
Yotsuha <32481230+Yotsuha@users.noreply.github.com>
2020-09-07 23:21:50 +0800

    Update config.py

--- config.py
+++ config.py
@@ -2,5 +2,5 @@
 client_id = 	6d5aeb3ec30728d
 client_secret = 414cbcd8f21ed4ff7896f561c24303ad1d0b0275
 album_id = 12345
-access_token = 
-refresh_token = 
+access_token = dd5c6c043d47274ee27383dc8bb2c495294edab2
+refresh_token = 6fa325763475d996ffab600c7dcd7a281edbb362
