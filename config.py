49867a7666858b405b8c1cec157557dfb6b53454
Yotsuha <32481230+Yotsuha@users.noreply.github.com>
2020-09-08 19:08:55 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # imgur key
-client_id = '6d5aeb3ec30728d'
-client_secret = '414cbcd8f21ed4ff7896f561c24303ad1d0b0275'
+client_id='fdd30319757d1cf'
+client_secret = '6f42e5cd3880343dc226903fcba6e235122a34d2'
 album_id = '12345'
-access_token = 'dd5c6c043d47274ee27383dc8bb2c495294edab2'
-refresh_token = '6fa325763475d996ffab600c7dcd7a281edbb362'
+access_token = '8cbb68dc05634ac04c9702efc3b149fee8d45808'
+refresh_token = 'a6fa653e7c5ad987953494a5ad70e10f5a6bda1e'
